5a800b479babb745b606ac80c236b454b83b245a
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-02 09:36:00 +0400

    Main

---
+++ Hello_world_001/Hello_User/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя");
+        string username = Console.ReadLine();
+        Console.Write("Привет, ");
+        Console.Write(username);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
+}
